4582a9b2e84bd258456a17073184261cc381f3c0
manuaba42 <ssedanayoga3@gmail.com>
2023-03-30 23:49:36 +0800

    update deal card

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 import os
+import random
 logo = """
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -9,8 +10,9 @@ logo = """
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-play = ("Do you want to play? 'y' or 'n': ")
+play = input("Do you want to play? 'y' or 'n': ")
 if play == 'y':
     clear = lambda: os.system('clear')
     clear()
@@ -19,8 +21,32 @@ else:
 
 print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    randoTrader = random.randint(0, len(cards)-1)
+    num = cards[randoTrader]
+    cards.pop(randoTrader)
+    return num
+
+def calculate_score(arr):
+    sums = sum(arr)
+    if sums > 21:
+        for index, num in enumerate(arr):
+            if num == 11:
+                arr.pop(index)
+                arr.append(1)
+
+    return sum(arr)
+
 
 condi = True
-# while condi == True:
+trader = []
+player = []
+for i in range(0,2):
+    numPlayer = deal_card()
+    player.append(numPlayer)
 
+scorePlayer = calculate_score(player)
+print(f"Your card: {player}, current score is: {scorePlayer}")
+trader.append(deal_card())
+print(f"Computer card is {trader}")
+# while condi == True:
